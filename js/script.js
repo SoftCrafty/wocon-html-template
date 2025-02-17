98c65f76266bd834bbc2ae616286883e438c371c
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-02-17 23:08:21 +0600

    17-02-2025

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+
+
 $(document).ready(function () {
     $(".tab").click(function () {
         $(".tab").removeClass("active");
         $(this).addClass("active");
+    });
+
+    // Off-Canvas Menu
+    $(".menu_btn").on("click", function () {
+        $(".off_canvas").addClass("active_offcanvas");
+    });
+
+    $(".cross_btn").on("click", function () {
+        $(".off_canvas").removeClass("active_offcanvas");
     });
 });
 
@@ -14,5 +25,25 @@ plusBtn.addEventListener("mouseenter", function(){
 });
 plusBtn.addEventListener("mouseleave", function(){
     socialIcon.classList.remove("active_icon");
+});
+
+
+$('.review_content').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+    nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                // dots: true,
+            }
+        }
+    ]
 });
 
