d91c9dd0682c309b1f2a5569f2afdad6046540d6
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-02-19 17:10:23 +0600

    19-02-2025

--- js/home2.js
+++ js/home2.js
@@ -3,16 +3,21 @@ $('.images_slider').slick({
     slidesToScroll: 1,
     prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
     nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                // dots: true,
             }
         }
     ]
+});
+$('.slider_parent').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+    nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
 });
