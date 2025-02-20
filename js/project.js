a5f48cffb50c83e2c1ed2de256785116fd9fb921
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-02-20 18:45:02 +0600

    20-02-2025

---
+++ js/project.js
@@ -0,0 +1,18 @@
+$('.review_content').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+    nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                // dots: true,
+            }
+        }
+    ]
+});
