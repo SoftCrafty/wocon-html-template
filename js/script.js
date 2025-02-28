714e331957bfad51b36b20ce727e14bee2698ae7
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-03-01 01:27:02 +0600

    All Page

--- js/script.js
+++ js/script.js
@@ -1,210 +1,147 @@
-
-
 $(document).ready(function () {
+    // Tab Active Class Toggle
     $(".tab").click(function () {
         $(".tab").removeClass("active");
         $(this).addClass("active");
     });
 
-    // Off-Canvas Menu
-    // $(".menu_btn").on("click", function () {
-    //     $(".menu-sidebar-area").addClass("menu-sidebar-area.active");
-    // });
-
-    // $(".cross_btn").on("click", function () {
-    //     $(".menu-sidebar-area").removeClass("menu-sidebar-area.active");
-    // });
-
-    // $(".mobile-menu-bar").on("click", function () {
-    //     $(".menu-sidebar-area").addClass("menu-sidebar-area.active");
-    // });
-
-    // $(".cross_btn").on("click", function () {
-    //     $(".menu-sidebar-area").removeClass("menu-sidebar-area.active");
-    // });
-   
-
-    /*
-      Jquery Sidebar Toggle
-      ============================*/
-    $(".mobile-menu-toggle-btn").on("click", function () {
-        $(".menu-sidebar-area").addClass("active");
-        $(".body-overlay").addClass("active");
-    });
-    $(".menu-sidebar-close-btn").on("click", function () {
-        $(".menu-sidebar-area").removeClass("active");
-        $(".body-overlay").removeClass("active");
+    // Sidebar (Off-Canvas Menu) Toggle
+    $(".mobile-menu-toggle-btn, .mobile-menu-bar").on("click", function () {
+        $(".menu-sidebar-area, .body-overlay").addClass("active");
     });
 
-    /*
-       Jquery Body Overlay
-       ============================*/
-    $(".body-overlay").on("click", function () {
-        $(".menu-sidebar-area").removeClass("active");
-        $(".body-overlay").removeClass("active");
+    $(".menu-sidebar-close-btn, .cross_btn, .body-overlay").on("click", function () {
+        $(".menu-sidebar-area, .body-overlay").removeClass("active");
     });
-});
 
+    // Sticky Navbar
+    function addStickyNav(navSelector, scrollOffset = 120, activeClass = 'active') {
+        let nav = $(navSelector);
+        $(window).on('scroll', function () {
+            $(window).scrollTop() > scrollOffset ? nav.addClass(activeClass) : nav.removeClass(activeClass);
+        });
+    }
+    addStickyNav('#top_nav');
+    addStickyNav('#top_home_nav'); // Home page er navbar
 
-let plusBtn = document.querySelector(".plus_btn");
-let socialIcon = document.querySelector(".social_icon");
+    // Header Scroll Effect
+    $(window).on("scroll", function () {
+        $("#header-area").toggleClass("scrolled", $(window).scrollTop() > 100);
+    });
 
-plusBtn.addEventListener("mouseenter", function(){
-    socialIcon.classList.add("active_icon");
-});
-plusBtn.addEventListener("mouseleave", function(){
-    socialIcon.classList.remove("active_icon");
-});
-
-
-$('.review_content').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
-    nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                // dots: true,
-            }
-        }
-    ]
-});
-
-
-$(document).ready(function () {
-    $('.logo-carousel').slick({
-        slidesToShow: 6,      
-        slidesToScroll: 1,        
+    // Slick Carousel - Review Content
+    $('.review_content').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+        nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
         autoplay: true,
-        autoplaySpeed: 1000,    
-        arrows: false,       
-        dots: false,            
-        infinite: true,
-        centerMode: true,       
-        centerPadding: "0px",
-        initialSlide: 2   ,
+        autoplaySpeed: 2000,
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    // dots: true,
-                }
-            }
+            { breakpoint: 1024, settings: { slidesToShow: 1, slidesToScroll: 1 } }
         ]
     });
-});
 
+    // Slick Carousel - Logo Section
+    $('.logo-carousel').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        centerPadding: "0px",
+        initialSlide: 2,
+        responsive: [
+            { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } }
+        ]
+    });
 
+    // Slick Carousel - Images Slider
+    $('.images_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+        nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            { breakpoint: 1024, settings: { slidesToShow: 1, slidesToScroll: 1 } }
+        ]
+    });
 
-$(document).ready(function () {
+    // Slick Carousel - albert_slider
+    $('.albert_slider_contains').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+        nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+    });
+
+    // Counter Animation
     function startCounter() {
-        $('.counter').each(function () {
-            var $this = $(this),
-                countTo = $this.attr('data-count');
-
-            $({ countNum: $this.text() }).animate(
-                { countNum: countTo },
-                {
-                    duration: 2000,
-                    easing: 'linear',
-                    step: function () {
-                        $this.text(Math.floor(this.countNum) + "+");
-                    },
-                    complete: function () {
-                        $this.text(this.countNum + "k+");
-                    }
-                }
-            );
+        $(".counter").each(function () {
+            let $this = $(this), countTo = $this.attr("data-count");
+            $({ countNum: $this.text() }).animate({ countNum: countTo }, {
+                duration: 2000,
+                easing: "linear",
+                step: function () { $this.text(Math.floor(this.countNum) + "+"); },
+                complete: function () { $this.text(this.countNum + "k+"); }
+            });
         });
     }
 
     function isScrolledIntoView(elem) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        return elemTop <= docViewBottom;
+        let docViewBottom = $(window).scrollTop() + $(window).height();
+        return $(elem).offset().top <= docViewBottom;
     }
 
-    $(window).on('scroll', function () {
-        if (isScrolledIntoView('.counter') && !$('.counter').hasClass('counted')) {
-            $('.counter').addClass('counted');
+    $(window).on("scroll", function () {
+        if (isScrolledIntoView(".counter") && !$(".counter").hasClass("counted")) {
+            $(".counter").addClass("counted");
             startCounter();
         }
     });
 
-
+    // GSAP Animations
     gsap.registerPlugin(ScrollTrigger);
 
     gsap.from(".experts_person_child", {
-        scrollTrigger: {
-            trigger: ".experts_person",
-            start: "top 80%",
-            toggleActions: "play none none reverse",
-        },
-        opacity: 0,
-        y: 50,
-        duration: 1,
-        stagger: 0.3,
+        scrollTrigger: { trigger: ".experts_person", start: "top 80%", toggleActions: "play none none reverse" },
+        opacity: 0, y: 50, duration: 1, stagger: 0.3,
     });
-
-    gsap.registerPlugin(ScrollTrigger);
 
     gsap.from(".blog_box", {
-        scrollTrigger: {
-            trigger: ".blog_box_parent",
-            start: "top 80%",
-            toggleActions: "play none none reverse",
-        },
-        opacity: 0,
-        y: 50,
-        duration: 1,
-        stagger: 0.3,
+        scrollTrigger: { trigger: ".blog_box_parent", start: "top 80%", toggleActions: "play none none reverse" },
+        opacity: 0, y: 50, duration: 1, stagger: 0.3,
+    });
+    gsap.from(".child_box", {
+        scrollTrigger: { trigger: ".our_teams_box", start: "top 80%", toggleActions: "play none none reverse" },
+        opacity: 0, y: 50, duration: 1, stagger: 0.3,
     });
 
-    
+    // Social Icon Hover Effect
+    $(".plus_btn").on("mouseenter", function () {
+        $(".social_icon").addClass("active_icon");
+    }).on("mouseleave", function () {
+        $(".social_icon").removeClass("active_icon");
+    });
 
 });
-
-
-window.addEventListener("scroll", function () {
-    let navbar = document.getElementById("header-area");
-
-    if (window.scrollY > 100) { 
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled"); 
-    }
-});
-
 $(document).ready(function () {
-    let nav = $('#top_nav');
+    $(".dropdown_btn").click(function () {
+        // Get the dropdown associated with the clicked button
+        var $dropdown = $(this).closest("li").find(".dropdown");
 
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 120) {
-            nav.addClass('active');
+        // If the dropdown is currently visible (has active class), hide it
+        if ($dropdown.hasClass("active")) {
+            $dropdown.removeClass("active");
         } else {
-            nav.removeClass('active');
+            // Otherwise, hide all dropdowns and show the clicked one
+            $(".dropdown").removeClass("active");
+            $dropdown.addClass("active");
         }
     });
 });
-
-
-
-// let nav = document.querySelector('#top_nav');
-
-// window.addEventListener('scroll', function () {
-//     if (window.scrollY > 120) {
-//         nav.classList.add('active');
-//     } else {
-//         nav.classList.remove('active');
-//     }
-// });
-
